b755386e0e6c17800cc84e7f40ca2f2bd037feef
ej4y <dlwodud821@proton.me>
2026-05-30 01:06:29 +0900

    feat: read input

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char* c = "Hello, World!";
-    printf("%s", c);
+    char input[256];
+
+    while(1) {
+        printf("> ");
+
+        // 한 줄 읽기
+        fgets(input, sizeof(input), stdin); // 버퍼, 최대 읽기 비트, 스트림
+
+        printf("You typed: %s\n", input);
+    }
 
     return 0;
 }
